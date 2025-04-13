fe804a7f2c92f0d4b64690e27bbdf82379054fde
amith kulkarni <amithkulkarni98@gmail.com>
2025-04-13 22:52:12 +0530

    moved all crdetials to .emv file

--- quera_clone/settings.py
+++ quera_clone/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +150,12 @@ SIMPLE_JWT = {
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
     "TOKEN_BLACKLIST_ENABLED": True,
 }
+
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
--- users/utils.py
+++ users/utils.py
@@ -1,29 +1,22 @@
 import smtplib
 from email.message import EmailMessage
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'amithtalentplace@gmail.com'
-EMAIL_HOST_PASSWORD = 'kxst rloj hbai yrpo' 
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'amithtalentplace@gmail.com'
+from django.conf import settings
 
 
 def send_password_email(to_email,password):
     subject = 'Your account Password'
-    body = f'Your password is {password}'
+    body = f'Your password is {password}, You can login with either Phone Number or Email'
     
     msg = EmailMessage()
     msg.set_content(body)
     msg['Subject'] = subject
-    msg['From'] = EMAIL_HOST_USER
+    msg['From'] = settings.EMAIL_HOST_USER
     msg['To'] = to_email
     
-    smtp_server = EMAIL_HOST
-    smtp_port = 587
-    smtp_user = EMAIL_HOST_USER
-    smtp_password = EMAIL_HOST_PASSWORD
+    smtp_server = settings.EMAIL_HOST
+    smtp_port = settings.EMAIL_PORT
+    smtp_user = settings.EMAIL_HOST_USER
+    smtp_password = settings.EMAIL_HOST_PASSWORD
     
     with smtplib.SMTP(smtp_server, smtp_user) as server:
         server.starttls()
